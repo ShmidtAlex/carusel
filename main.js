8aaa005e6b1fdd47d9df0efa6767423521acc1a6
Alexander Shmidt <shmidt.alex@gmail.com>
2019-02-10 17:50:18 +0300

    variable names corrected, redundant code eliminated

--- main.js
+++ main.js
@@ -2,36 +2,36 @@ let imageIndex = 0;
 //get access to "container" div
 let nodesList = document.body.children[0];
 //create new div class mainBlock and incert it at the begining of the child nodes list
-let mainblocknode = document.createElement('div');
-nodesList.appendChild(mainblocknode);
-mainblocknode.setAttribute('class', 'mainBlock');
+let mainBlockNode = document.createElement('div');
+nodesList.appendChild(mainBlockNode);
+mainBlockNode.setAttribute('class', 'mainBlock');
 //create new inputs and div.pictureBlock
-let leftbutton = document.createElement('input');
-let rightbutton = document.createElement('input');
-let pictureblock = document.createElement('div');
+let leftButton = document.createElement('input');
+let rightButton = document.createElement('input');
+let pictureBlockNode = document.createElement('div');
 //set up attributes of mainBlock child nodes
-leftbutton.setAttribute('type', 'button');
-rightbutton.setAttribute('type', 'button');
-leftbutton.setAttribute('class', 'arrow left');
-rightbutton.setAttribute('class', 'arrow right');
-pictureblock.setAttribute('class', 'pictureBlock');
+leftButton.setAttribute('type', 'button');
+rightButton.setAttribute('type', 'button');
+leftButton.setAttribute('class', 'arrow left');
+rightButton.setAttribute('class', 'arrow right');
+pictureBlockNode.setAttribute('class', 'pictureBlock');
 //put above elements into div.mainBlock
-mainblocknode.appendChild(leftbutton);
-mainblocknode.appendChild(pictureblock);
-mainblocknode.appendChild(rightbutton);
+mainBlockNode.appendChild(leftButton);
+mainBlockNode.appendChild(pictureBlockNode);
+mainBlockNode.appendChild(rightButton);
 //create div.indicator and set it's attributes
-let divindicator = document.createElement('div');
+let divIndicator = document.createElement('div');
 //add div.indicator as a last child in nodesList
-nodesList.appendChild(divindicator).setAttribute('class', 'indicator');;
+nodesList.appendChild(divIndicator).setAttribute('class', 'indicator');;
 //create list of indicator items
-let inditems = document.createElement('ul');
-inditems.setAttribute('id', 'indicatorList');
-divindicator.appendChild(inditems);
+let indItems = document.createElement('ul');
+indItems.setAttribute('id', 'indicatorList');
+divIndicator.appendChild(indItems);
 for (let i = 0; i < 6; i++){
   let item = document.createElement('li');
-  inditems.appendChild(item);
+  indItems.appendChild(item);
 }
-inditems.childNodes[imageIndex].setAttribute('id', 'start');
+indItems.childNodes[imageIndex].setAttribute('id', 'start');
 //array with photo
 const picArr = [
   'golden-retriever-puppy.jpg',
@@ -52,35 +52,33 @@ function showImage(imageIndex, containerElement){
   //get list of indicators
   let indicatorList = document.getElementById('indicatorList');
   //delete list of indicators
-  divindicator.removeChild(indicatorList);
+  divIndicator.removeChild(indicatorList);
   //recreate list of indicators and set it's attributes
-  let inditems = document.createElement('ul');
-  divindicator.appendChild(inditems).setAttribute('id', 'indicatorList');
+  let indItems = document.createElement('ul');
+  divIndicator.appendChild(indItems).setAttribute('id', 'indicatorList');
     for (let i = 0; i < 6; i++){
     let item = document.createElement('li');
-    inditems.appendChild(item);
+    indItems.appendChild(item);
   }
   //change colour of running indicator item
-  inditems.childNodes[imageIndex].setAttribute('id', 'start');
+  indItems.childNodes[imageIndex].setAttribute('id', 'start');
 }
 
 function showNextPic(){  
   if(imageIndex < picArr.length-1) {
-    imageIndex++;
-    showImage(imageIndex, pictureBlock);
+    imageIndex++;    
   } else {
-    imageIndex = 0;
-    showImage(imageIndex, pictureBlock);
+    imageIndex = 0;   
   }  
+  showImage(imageIndex, pictureBlock);
 }
 function showPreviousPic(){
   if (imageIndex > 0){
-    imageIndex--;
-    showImage(imageIndex, pictureBlock);
+    imageIndex--;   
   } else {
     imageIndex = picArr.length-1;
-    showImage(imageIndex, pictureBlock);
   }    
+  showImage(imageIndex, pictureBlock);
 }
 
 let idInterval;
